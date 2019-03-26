910ff31a7d236e9669becbb91ed69f8e3a57a037
Turzhanskyi <turzhansky81@gmail.com>
2019-03-26 08:24:03 +0200

    #2 React - локальний state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import Dropdown from './Dropdown.js';
+
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+        <div>
+          <Dropdown />
+        </div>
+    );
   }
 }
+
 export default App;
