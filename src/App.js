63c168194251d803f6b356ad1b999494adcaf2c3
Turzhanskyi <turzhansky81@gmail.com>
2019-03-26 15:04:57 +0200

    #5 React - форми реєстрації

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
 
-import Header from './Header';
-
-const menu = [
-    {
-        link: '/articles',
-        label: 'Articles'
-    },
-    {
-        link: '/contacts',
-        label: 'Contacts'
-    },
-    {
-        link: '/posts',
-        label: 'Posts'
-    }
-];
+import RegistrationForm from './RegisttationForm';
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-          <Header items={menu}/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <RegistrationForm />
+            </div>
+        );
+    }
 }
 
 export default App;
