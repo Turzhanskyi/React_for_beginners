60e91cf2e96f0cc9918c91cfe8cc62ea3d716671
Turzhanskyi <turzhansky81@gmail.com>
2019-03-26 16:22:58 +0200

    #6 React css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import "./App.css";
 import RegistrationForm from './RegisttationForm';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <RegistrationForm />
             </div>
         );
